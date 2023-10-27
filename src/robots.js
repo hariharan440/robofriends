b2d9882f87b21b8113185a666ac535bf5ff48e5c
hari <hariharan02anantharaj@gmail.com>
2023-10-27 23:55:40 +0530

    first commit

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Hari',
+        username: 'irah',
+        email: 'hari@gmail.com'
+    },
+    {
+        id: 2,
+        name: 'durga',
+        username: 'drah',
+        email: 'durga@gmail.com'
+    },
+    {
+        id: 3,
+        name: 'ari',
+        username: 'rah',
+        email: 'ari@gmail.com'
+    },
+    {
+        id: 4,
+        name: 'raja',
+        username: 'rajah',
+        email: 'raja@gmail.com'
+    },
+    {
+        id: 5,
+        name: 'lax',
+        username: 'laxrah',
+        email: 'lax@gmail.com'
+    },
+    {
+        id: 6,
+        name: 'laxa',
+        username: 'laaxrah',
+        email: 'laax@gmail.com'
+    },
+    {
+        id: 7,
+        name: 'srinath',
+        username: 'srinath',
+        email: 'srix@gmail.com'
+    },
+    {
+        id: 8,
+        name: 'sathish',
+        username: 'sathishh',
+        email: 'sathish@gmail.com'
+    },
+    {
+        id: 9,
+        name: 'rabin',
+        username: 'rabin',
+        email: 'rabin@gmail.com'
+    },
+    {
+        id: 10,
+        name: 'hariii',
+        username: 'harii',
+        email: 'hariii@gmail.com'
+    },
+]
